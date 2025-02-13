efa17df77fcabdc404b408e6638466da9e317a8b
Jaya Winata <jayawinata100@gmail.com>
2025-02-13 10:31:00 +0700

    feat: create unzip script

--- elt_script/elt_script.py
+++ elt_script/elt_script.py
@@ -7,8 +7,33 @@ import gzip
 import shutil
 import os
 
-def unzip(url):
-    filename = 
+def unzip(url: str):
+    filename = url.split('/')[-1]
     try:
-        response = requests.get(url)
         print(f'Downloading {filename}')
+        response = requests.get(url)
+        response.raise_for_status()
+
+        print(f'Writing {filename}')
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+
+        if os.path.splitext(filename)[1].lower() == ".zip":
+            csv_name = ''.join(filename.split('.')[:-1])
+            print(f'Extracting {filename}')
+            with gzip.open(filename, 'rb') as f_in:
+                with open('../dataset/' + csv_name, 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+    except Exception:
+        raise Exception('File Unzipping Error')
+
+    print(f"File downloaded and extracted to: {os.getcwd()}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--url', help='url for the csv file')
+    args = parser.parse_args()
+    unzip(args)
+
+
